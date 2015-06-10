edb7fa21ca9e9986d3de83f2ae7b9394ac54752a
Hussien <akhayoon@gmail.com>
2015-06-10 10:30:39 -0400

    Added timer and commented on code more

--- editor/views.py
+++ editor/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse,HttpResponseRedirect
 import json
 
 
-# Create your views here.
+# this view simply tries to grab any html or css values from the session
+# whether they exist or not we'll pass them on as variables to the template
 def displayEditor(request):
     
     html = request.session.get('html', '')
@@ -14,23 +15,26 @@ def displayEditor(request):
     return render_to_response('editor/home.phtml', {'html': html,'css': css},
                           context_instance=RequestContext(request))
 
+# Here is where we extract our JSON data and store
+# it in the session
 @csrf_exempt
 def saveData(request):
     json_data   = json.loads(request.body)
-    html    = json_data.get('html', '')
-    css     = json_data.get('css', '')
+    html        = json_data.get('html', '')
+    css         = json_data.get('css', '')
     
     request.session['html'] = html
     request.session['css'] = css
     
     return HttpResponse()
 
+# this method takes our session variables and put them
+# into variables and passed on into a template
 @csrf_exempt
 def render(request):
         
     html = request.session.get('html', '')
     css  = request.session.get('css', '')
     
-
     return render_to_response('editor/render.phtml', {'html': html,'css': css},
                       context_instance=RequestContext(request))
--- static/js/data.js
+++ static/js/data.js
@@ -1,17 +1,26 @@
 angular.module('liveEditorApp', [])
 
-	.controller('dataController', ['$scope', '$http', function($scope, $http){
+	.controller('dataController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout){
 		
+
+		// Here we initialize our data variable that we're sending to our view
 		$scope.data = {};
 		
-		
+		/*
+		 * This method is called on ng-click on one of the buttons.
+		 * This is how we're going to be sending our data to our view.
+	 	 * Note that we use this method to both save the data and launch our rendering.
+	 	 * The reason is that we save in either case, we add the launch flag to distinguish 
+	 	 * between the two
+	 	 */
 		$scope.save = function(launch){
 			
 			$scope.data.html = window.html;
 			$scope.data.css = window.css;
+			
+			// our flag to distinguish a save or a launch
 			$scope.data.launch = launch;
 			
-
 			$http.post('/save', {html:html, css:css, launch:launch}).
 			success(function(data) {
 				console.log('success!!!');
@@ -19,6 +28,15 @@ angular.module('liveEditorApp', [])
 				if (launch == 1){
 					console.log('Rendering...');
 					window.open('/render', '_blank');
+				}
+				else {
+					$scope.showMessage = true;
+
+				      
+			        $timeout(function() {
+			          $scope.showMessage = false;
+			        }, 3000);
+
 				}
 				
 			}).
